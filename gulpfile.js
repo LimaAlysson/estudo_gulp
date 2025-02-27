ac049c8622c42bb9bfcbff87e0aba6ed133dd7a6
Alysson <alyssonredes32@gmail.com>
2025-02-26 22:22:50 -0300

    feature/tasks paralelas

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 const gulp = require('gulp'); //importando o pacote gulp
 
 function funcaoPadrao(callback) {
-    console.log('Função Padrão :::: Executando via Gulp');
-    callback();
+    setTimeout(function() {
+        console.log('Função Padrão :::: Executando via Gulp');
+        callback();
+    }, 2000);
+    
 }
 
 function soma(callback) {
-    console.log('Função Soma :::: 1 + 1 =  ', 1+1);
-    dizTchau();
-    callback();
+    setTimeout(function() {
+        console.log('Função Soma :::: 1 + 1 =  ', 1+1);
+        dizTchau();
+        callback();
+    }, 5000);
 }
 
 function dizTchau() {
     console.log('Função Tchau :::: Tchau Gulp!')
 }
 
-exports.default = gulp.series(funcaoPadrao, soma);
+// exports.default = gulp.series(funcaoPadrao, soma);
 // exports.soma = soma;
+
+exports.default = gulp.parallel(funcaoPadrao, soma);
