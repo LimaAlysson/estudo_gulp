d982edfbe5bb33c82886cf9930e5211677e88272
Alysson <alyssonredes32@gmail.com>
2025-03-03 11:46:12 -0300

    feature/minimizando e ofuscando js + mapenado css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,33 @@
-const gulp = require('gulp'); //importando o pacote gulp
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate')
+
+function comprimeJs() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+gulp.task('comprimejs', comprimeJs);
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+gulp.task('sass', compilaSass);
 
 function funcaoPadrao(callback) {
     setTimeout(function() {
         console.log('Função Padrão :::: Executando via Gulp');
         callback();
     }, 2000);
-    
 }
 
 function soma(callback) {
@@ -21,6 +43,12 @@ function dizTchau() {
 }
 
 // exports.default = gulp.series(funcaoPadrao, soma);
-// exports.soma = soma;
+//exports.soma = soma;
+//exports.default = gulp.parallel(funcaoPadrao, soma);
 
-exports.default = gulp.parallel(funcaoPadrao, soma);
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false}, gulp.series(compilaSass));
+}
+
+exports.comprimejs = comprimeJs;
